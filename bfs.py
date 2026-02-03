c5ad90fff767cc5b3b8fb86bb3850975173ba9b8
Usha-sundarapu <usundara@gitam.in>
2026-02-03 13:18:26 +0530

    Create bfs.py

---
+++ bfs.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque()
+
+    visited.add(start)
+    queue.append(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=" ")
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Example graph
+graph = {
+    0: [1, 2],
+    1: [0, 3, 4],
+    2: [0, 5],
+    3: [1],
+    4: [1],
+    5: [2]
+}
+
+bfs(graph, 0)
